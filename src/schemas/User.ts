49b4af47017d5932b908af382bf529042c4284d9
nicoledbianchin <nicoledbianchin@gmail.com>
2020-12-09 15:57:21 -0300

    build user schema

---
+++ src/schemas/User.ts
@@ -0,0 +1,11 @@
+import { Schema, Model, model } from 'mongoose'
+
+const UserSchema = new Schema({
+    email: String,
+    firstName: String,
+    lastName: String
+}, {
+    timestamps: true
+})
+
+export default model('User', UserSchema)
